1c674b1ee07685f1b890410483e6b92a73591bd0
Joe Fleming <joe.fleming@gmail.com>
2020-11-19 15:24:22 -0700

    chore: fix the destinations for other sites
    pretty sure i had this wrong...

--- sites/proxy/next.config.js
+++ sites/proxy/next.config.js
@@ -3,19 +3,19 @@ module.exports = {
     return [
       {
         source: '/url',
-        destination: 'https://url-params-sibi-mono-frontends.vercel.app/'
+        destination: 'https://url-params-sibi-mono-frontends.vercel.app/url/'
       },
       {
         source: '/url/:path*',
-        destination: 'https://url-params-sibi-mono-frontends.vercel.app/:path*'
+        destination: 'https://url-params-sibi-mono-frontends.vercel.app/url/:path*'
       },
       {
         source: '/echo',
-        destination: 'https://echo-test-sibi-mono-frontends.vercel.app/',
+        destination: 'https://echo-test-sibi-mono-frontends.vercel.app/echo/',
       },
       {
         source: '/echo/:path*',
-        destination: 'https://echo-test-sibi-mono-frontends.vercel.app/:path*',
+        destination: 'https://echo-test-sibi-mono-frontends.vercel.app/echo/:path*',
       },
     ]
   },
